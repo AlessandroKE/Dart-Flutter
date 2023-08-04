552d248349db586cf5a53beef57cac6e2fba5e6d
SandroKE <koomealessandro@gmail.com>
2023-08-04 15:21:59 +0300

    initial commit

---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  /* The var keyword is a shorthand way of declaring variables when you want the 
+  Dart compiler to automatically infer the type of the variable based on the value you assign to it. */
+  var emp_name = "Sandro KE"; // Compiler infers that name is of type String.
+
+  print("Welcome, $emp_name");
+  print("Welcome, " + emp_name + "!");
+/* when using string interpolation, the variables inside ${} will be automatically converted to strings,
+ so you don't need to explicitly call .toString() on them.
+} */
+
+}
