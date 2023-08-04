c7e16a786fb60e5cb392c76cea5d3d27ee2c2432
SandroKE <koomealessandro@gmail.com>
2023-08-04 15:33:09 +0300

    initial commit

--- variables.dart
+++ variables.dart
@@ -4,7 +4,7 @@ void main() {
   Dart compiler to automatically infer the type of the variable based on the value you assign to it. */
   var emp_name = "Sandro KE"; // Compiler infers that name is of type String.
 
-  print("Welcome, $emp_name");
+  print("Welcome, $emp_name " + "to dart Programming");
   print("Welcome, " + emp_name + "!");
 /* when using string interpolation, the variables inside ${} will be automatically converted to strings,
  so you don't need to explicitly call .toString() on them.
---
+++ variables2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var emp_name = "Sandro KE";
+  var emp_age = "22";
+  var emp_salary = "10000";
+//when adding a dollar sign use \$ sign.
+  print("My name is ${emp_name} and my age is ${emp_age} and my salary is \$${emp_salary}");
+
+}
