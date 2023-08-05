b74371f2c210acb3c63dd82e274b72b6d7ac3b76
SandroKE <koomealessandro@gmail.com>
2023-08-05 22:40:26 +0300

    initial commit

---
+++ variables3.dart
@@ -0,0 +1,44 @@
+void main(){
+ /*  
+ numbers 
+  int emp_age;
+  double emp_age;
+
+ Strings
+  var emp_name;
+
+ Boolean
+  bool emp_flag;
+
+ lists
+  var emp_list = ['a', 'b', 'c', 'd', 'e];
+
+ maps
+
+ runes 
+ symbols
+    
+ 
+ 
+ 
+ 
+ 
+ */
+
+
+var employee = {
+  'name': 'John Doe',
+  'age': 30,
+  'salary': 10000.50,
+  'isEmployed': true,
+};
+
+print(employee['name']); // Output: 'John Doe'
+employee['bonus'] = 500;
+print(employee); // Output: {name: John Doe, age: 30, salary: 10000.50, isEmployed: true, bonus: 500}
+
+
+var heartRune = '\u2665';
+print(heartRune); // Output: '♥'
+
+}
