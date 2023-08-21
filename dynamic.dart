2060dbbf59e196d402a99baf61e414b85570276b
SandroKE <koomealessandro@gmail.com>
2023-08-22 01:28:42 +0300

    printing multiple lines of code

--- dynamic.dart
+++ dynamic.dart
@@ -5,6 +5,13 @@ void main() {
 
   dynamic age = 11;
 
+  //To print many lines of code use ''' '''
+
+  dynamic Sandro = ''' Hello world 
+  this is Alessandro
+  ''';
+  print(Sandro);
+
   // At compile time, the value is not known but at runtime the value is known.
 
   // the runtime property is acessed without any parenthesis. Use a dot instead
