27889fb0233ac01ad8496a55f60d268ec6ab5dac
SandroKE <koomealessandro@gmail.com>
2023-08-26 14:44:21 +0300

    updated

--- Control Flow/ternary.dart
+++ Control Flow/ternary.dart
@@ -5,9 +5,13 @@ void main() {
   if (somevalue.startsWith('H')) {
     print("Hello World this is $name");
     print("Hello World this is " + name);
-  }else{
-
+  } else {
     print(name + " is the next big value");
-
   }
+
+  //Ternary
+
+  String value = somevalue.startsWith('H') ? 'wow' : 'naah';
+
+  print(value);
 }
