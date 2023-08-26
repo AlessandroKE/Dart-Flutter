4cbb6e9f774354d3e449e782d78014c0274c2665
SandroKE <koomealessandro@gmail.com>
2023-08-26 23:23:45 +0300

    initial commit

--- Functions/functions1.dart
+++ Functions/functions1.dart
@@ -1,9 +1,22 @@
 
-//executing the function:
+/* //executing the function:
 void main() {
   print(someValue());
 }
 //Creating fucnction of the typr string
 String someValue() {
   return("Alessandro is a Guru");
+}
+ */
+
+void main() {
+  //Re
+  String greet(String name) 
+  {
+    return "Hello, $name!";
+  }
+
+  String person = "Alice";
+  String greeting = greet(person);
+  print(greeting);
 }
---
+++ Functions/tempCodeRunnerFile.dart
@@ -0,0 +1,8 @@
+//executing the function:
+void main() {
+  print(someValue());
+}
+//Creating fucnction of the typr string
+String someValue() {
+  return("Alessandro is a Guru");
+}
