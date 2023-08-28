39f4bcde7990efd22ae6ee9f1934d255ac0d786d
SandroKE <koomealessandro@gmail.com>
2023-08-28 13:31:29 +0300

    initial commit

---
+++ Functions/positional_argument.dart
@@ -0,0 +1,9 @@
+void main() {
+  //When a variable is defined inside a function its called a local varibale 
+  String name = 'Alessandro this is a good thing';
+  printName(name);
+}
+//when a variable is defined outside a function its called a global variable
+void printName(String sandro) {
+  print('Hello ' + sandro);
+}
