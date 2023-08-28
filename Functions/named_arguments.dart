f397e3d37416cfd0f9f75590750222c0e4fb7579
SandroKE <koomealessandro@gmail.com>
2023-08-28 15:07:41 +0300

    initial commit

--- Functions/named_arguments.dart
+++ Functions/named_arguments.dart
@@ -1,13 +1,27 @@
 
 //Working with named arguments.
 void main() {
+
+  //for named arguments you dont need to match the position of the arguments:
   String name = 'Alessandro';
 //callig the function
   printName(name: name, age: 12, greetings: 'Hello');
 }
 
 //name arguments must be declared within brackets {}
+
+//You can choose to have a variable  has a required or npt
+//I can have a variable has a null or optionl buy use of a question mark
+
+//NB: You can have both positional and named arguments in your code on the same line example:
+
+/* void printName(int salary,
+    {required String name, required int? age, required String greetings}) {
+  print(name);
+ */
+
 void printName(
-    {required String name, required int age, required String greetings}) {
+    {required String name, required int? age, required String greetings}) {
   print(name);
 }
+
